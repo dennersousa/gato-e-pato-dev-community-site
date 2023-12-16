720dea53205b8bb5a031433e342a3b67ee4e4ad0
marcuscarvalhodev <135276762+marcuscarvalhodev@users.noreply.github.com>
2023-12-16 17:27:55 -0300

    versão alpa 0.0.1: implementação - dark mode

--- src/script.js
+++ src/script.js
@@ -1,11 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var footer = document.getElementById("myFooter");
+const chk = document.getElementById("chk");
 
-    window.onscroll = function() {
-        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-            footer.style.display = "block";
-        } else {
-            footer.style.display = "none";
-        }
-    };
+chk.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+    const paragraphs = document.querySelectorAll("p, #paragrafo, #paragrafo1, #paragrafo2");
+    paragraphs.forEach(paragraph => {
+        paragraph.style.color = document.body.classList.contains("dark") ? "#fff" : "#000";
+    });
 });
+
+window.onscroll = function() {
+    const footer = document.getElementById("myFooter"); 
+    if (window.scrollY > 200 || document.documentElement.scrollTop > 200) {
+        footer.style.display = "block";
+    } else {
+        footer.style.display = "none";
+    }
+};
+
